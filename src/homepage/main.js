c66b322f0418ef70f639a674081e7ba9af29b1af
justinnotoma <justinhot906@gmail.com>
2025-03-17 23:16:07 -0400

    Code for display a number on the timer

--- src/homepage/main.js
+++ src/homepage/main.js
@@ -1,3 +1,26 @@
 import './style.css'
 
-// 
+/**
+ * Display a number of the timer
+ * @param {Number} num 
+ */
+function displayNum(num) {
+    const numDigits = num.toString().split('')
+    const digitWrappers = document.getElementsByClassName('digit-wrapper')
+
+    const startingDigit = 7 - numDigits.length
+    
+    numDigits.forEach((digit, index) => {
+        const numShift = parseInt(digit) * -92.5
+        digitWrappers[index + startingDigit].style.transform = `translateY(${numShift}px)`
+    })
+}
+
+// setInterval(() => {
+//     const randomNum = Math.floor( Math.random() * 9999999 )
+//     displayNum(randomNum)
+// }, 3000);
+
+    
+displayNum(264719)
+// displayNum(999999)
