d0ca031d8135c69d46688099df880017b086b2f0
justinnotoma <justinhot906@gmail.com>
2025-03-21 16:35:47 -0400

    update getUserInfo function

--- functions/getUserInfo.js
+++ functions/getUserInfo.js
@@ -1,4 +1,23 @@
+import { Client, Databases } from "node-appwrite"
+
+const PROJECT_ID = process.env.PROJECT_ID
+const DB_ID = process.env.DB_ID
+const COLLECTION_USERINFO_ID = process.env.COLLECTION_USERINFO_ID
 
 export default async ({req, res, log, error}) => {
-    return res.send('Hello World')
+    const client = new Client()
+    client.setEndpoint('https://cloud.appwrite.io/v1')
+    client.setProject(PROJECT_ID)
+
+    const db = new Databases(client)
+
+    if (req.method == 'GET') {
+        const respone = await db.listDocuments(
+            DB_ID,
+            COLLECTION_USERINFO_ID
+        )
+        
+    return res.json(respone.documents)
+    }
+
 }
