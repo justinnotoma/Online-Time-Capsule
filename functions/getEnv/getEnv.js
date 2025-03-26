a8a97f95c23639b965bc25d445c2b0a0059dbf0a
justinnotoma <justinhot906@gmail.com>
2025-03-26 15:12:32 -0400

    bug fixing

--- functions/getEnv/getEnv.js
+++ functions/getEnv/getEnv.js
@@ -2,4 +2,5 @@ const projectID = process.env.project_ID
 
 export default async ({req, res, logs, error}) => {
     if (req.method == 'GET') res.json( {"prjectID": projectID} )
+    else res.json( {'error': "Error 405: must be a GET request"} )
 }
