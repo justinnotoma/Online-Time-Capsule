39d9353744226d62df6cf7bf491adba4f21ef740
justinnotoma <justinhot906@gmail.com>
2025-03-26 16:26:21 -0400

    deleted "getEnv" function

--- functions/getEnv/getEnv.js
+++
@@ -1,6 +0,0 @@
-const projectID = process.env.PROJECT_ID
-
-export default async ({req, res, logs, error}) => {
-    if (req.method == 'GET') return res.json( {"prjectID": projectID} )
-    else return res.json( {'error': "Error 405: must be a GET request"} )
-}
