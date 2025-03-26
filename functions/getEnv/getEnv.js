1af654e9de9802a72b8bac7ffb3321b7444af76c
justinnotoma <justinhot906@gmail.com>
2025-03-26 15:14:28 -0400

    added missing return statements

--- functions/getEnv/getEnv.js
+++ functions/getEnv/getEnv.js
@@ -1,6 +1,6 @@
 const projectID = process.env.project_ID
 
 export default async ({req, res, logs, error}) => {
-    if (req.method == 'GET') res.json( {"prjectID": projectID} )
+    if (req.method == 'GET') return res.json( {"prjectID": projectID} )
     else return res.json( {'error': "Error 405: must be a GET request"} )
 }
